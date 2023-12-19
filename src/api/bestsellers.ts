a73f8d52a22ed630c2c27e98a1d42be8d6895cc4
ga-in-for-company <nopainogaini@naver.com>
2023-12-20 07:27:18 +0900

    feat:api 호출코드 작성중

---
+++ src/api/bestsellers.ts
@@ -0,0 +1,5 @@
+export const loadBestSellerListFromA = ()=>{
+  
+}
+
+//https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=&QueryType=Bestseller&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101&Cover=MidBig&OPtResult=ebookList,usedList
